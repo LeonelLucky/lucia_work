985a8bc7717b1e9ef61700fb086db9bea82f56a5
ViniciusMazetto <vinifidelis3001@gmail.com>
2025-04-03 11:42:54 -0300

    agora funciona o negocio de jogar denovo
    ve se a lucia deixa usar isso ne

--- leonas.py
+++ leonas.py
@@ -1,12 +1,61 @@
 import random
+lucia = 's'
+while lucia != 'n':
+    numero_secreto = random.randint(1000, 9999)
+    n1 = numero_secreto // 1000
+    n2 = (numero_secreto % 1000) // 100
+    n3 = (numero_secreto % 100) // 10
+    n4 = numero_secreto % 10
 
-numerosecreto = random.randint(1000, 9999)
-numerosecreto = str(numerosecreto)
-a = numerosecreto[0]
-b = numerosecreto[1]
-c = numerosecreto[2]
-d = numerosecreto[3]
-print(a, b, c, d)
-print(f'O numero secreto é {numerosecreto}')
+    traco1 = -1
+    traco2 = -1
+    traco3 = -1
+    traco4 = -1
 
+    print(" => Bem-vindo Ao Jogo SECRETO!!! <=")
+    print("Você tem 10 tentativas para acertar um número entre [1000 e 9999].")
+    print("A partir da 5ª tentativa, o jogo dará dicas mostrando quais dígitos estão corretos.")
+    print("Caso queira parar de jogar, tecle CTRL + C!")
+    input("<<< Tecle Algo >>>")
+    print('-----------------------------')
 
+    tentativa = 1
+    while tentativa <= 10:
+        palpite = float(input(f"Tentativa {tentativa} - Digite seu palpite: "))
+        inNumberint = int(palpite)
+        if palpite == inNumberint:
+            print ("")
+        else:
+            print ("Numero Inválido! Presta atenção!")
+            tentativa -= 1
+        if palpite < 1000 or palpite > 9999:
+            print('Entrada Invalida!')
+            tentativa -= 1
+
+        c1 = palpite // 1000
+        c2 = (palpite % 1000) // 100
+        c3 = (palpite % 100) // 10
+        c4 = palpite % 10
+        
+        if palpite == numero_secreto:
+            print(f"Parabéns! Você acertou o número secreto {numero_secreto:.0f} na {tentativa}ª tentativa!")
+            break
+        
+        print(f"Você digitou um número errado/inválido. Tente novamente!")
+        
+        if tentativa > 0:
+            if c1 == n1:
+                traco1 = n1
+            if c2 == n2:
+                traco2 = n2
+            if c3 == n3:
+                traco3 = n3
+            if c4 == n4:
+                traco4 = n4
+            print(traco1, traco2, traco3, traco4)
+        tentativa += 1  
+    if palpite != numero_secreto:
+        print(f"Você não acertou. O número secreto era {numero_secreto}.")
+    if tentativa > 10 or palpite == numero_secreto:
+        print("Fim do jogo!")
+    lucia=(input("Voce deseja jogar denovo ?(s/n)"))
