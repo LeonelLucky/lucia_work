49f5fc389cc91298f6f45af0910e8db64c962179
ViniciusMazetto <vinifidelis3001@gmail.com>
2025-04-02 22:01:07 -0300

    arrumei a tentativa invalida e tentei arrumar pra conseguir jogar novamente

--- main jogo.py
+++ main jogo.py
@@ -1,40 +1,58 @@
-c1 = int(2)
-c2 = int(0)
-c3 = int(0)
-c4 = int(6)
-n1= str("_")
-n2= str("_")
-n3= str("_")
-n4= str("_")
-# tela de inicio
+import random
 
-print('Bem-Vindo ao Jogo SECRETO!!!')
+numero_secreto = random.randint(1000, 9999)
+n1 = numero_secreto // 1000
+n2 = (numero_secreto % 1000) // 100
+n3 = (numero_secreto % 100) // 10
+n4 = numero_secreto % 10
 
-print('Você tem 10 tentativas para acertar o número secreto entre 1000 e 9999')
+traco1 = -1
+traco2 = -1
+traco3 = -1
+traco4 = -1
 
-print('A partir da 5a. tentativa o jogo irá te ajudar, dando dicas.')
-input('Tecle algo ')
-print('--------------------------------------------')
+print(" => Bem-vindo Ao Jogo SECRETO!!! <=")
+print("Você tem 10 tentativas para acertar um número entre [1000 e 9999].")
+print("A partir da 5ª tentativa, o jogo dará dicas mostrando quais dígitos estão corretos.")
+input("<<< Tecle Algo >>>")
+print('-----------------------------')
 
-tentativa = int(input('Seu código é: '))
-
-if (2006) == tentativa:
-    print('PARABENS VOCÊ ACERTOU!!!')
-else:
-    print('VOCÊ NÃO ACERTOU NADA NESSA TENTATIVA ...')
-    for i in range(10):
-        tentativa = int(input(f'{i + 1}ª tentativa'))
-        print (f'Seu código é {n1} {n2} {n3} {n4}')
-        
-        if c1==2:
-            n1=c1
-        if c2==0:
-            n2=c2
-        if c3==0:
-            n3=c3
-        if c4==6:
-            n4=c4
-        if (c1) and (c2) and (c3) and (c4) == tentativa:
-            print('PARABENS VOCÊ ACERTOU!!!')
+tentativa = 1
+while tentativa <= 10:
+    palpite = int(input(f"Tentativa {tentativa} - Digite seu palpite: "))
+    if palpite < 1000 or palpite > 9999:
+        print('Entrada Invalida!')
+        tentativa -= 1
+    
+    c1 = palpite // 1000
+    c2 = (palpite % 1000) // 100
+    c3 = (palpite % 100) // 10
+    c4 = palpite % 10
+    
+    if palpite == numero_secreto:
+        print(f"Parabéns! Você acertou o número secreto {numero_secreto} na {tentativa}ª tentativa!")
+        break
+    
+    print(f"Você digitou {palpite}. Tente novamente!")
+    
+    if tentativa > 0:
+        if c1 == n1:
+            traco1 = n1
+        if c2 == n2:
+            traco2 = n2
+        if c3 == n3:
+            traco3 = n3
+        if c4 == n4:
+            traco4 = n4
+        print(traco1, traco2, traco3, traco4)
+    
+    tentativa += 1  
+if palpite != numero_secreto:
+    print(f"Você não acertou. O número secreto era {numero_secreto}.")
+if tentativa > 10 or palpite == numero_secreto:
+    print("Fim do jogo!")
 
     
+
+
+
